0cc8e4be0b4d4b3909dd8e71f38aaebc8ee2a0be
Dinh Duc Hoang <149225882+dinhduchoang@users.noreply.github.com>
2026-04-18 09:54:42 +0700

    Add files via upload
    ma co dien

---
+++ MaCeasar.java
@@ -0,0 +1,77 @@
+public class MaCeasar {
+    private String banRo = "", banMa = "";
+    int khoa = 0;
+
+    public MaCeasar() {}
+
+    public int getKhoa() {
+        return khoa;
+    }
+
+    public String getBanRo() {
+        return banRo;
+    }
+
+    public String getBanMa() {
+        return banMa;
+    }
+
+    public void setBanRo(String banRo) {
+        this.banRo = banRo;
+    }
+
+    public void setBanMa(String banMa) {
+        this.banMa = banMa;
+    }
+
+    public void setKhoa(int khoa) {
+        this.khoa = khoa;
+    }
+
+    public String maHoa() {
+        StringBuilder ketQua = new StringBuilder();
+
+        for (int i = 0; i < banRo.length(); i++) {
+            char c = banRo.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                int viTriCu = c - 'a';
+                int viTriMoi = (viTriCu + khoa) % 26;
+                ketQua.append((char) (viTriMoi + 'a'));
+
+            } else if (c >= 'A' && c <= 'Z') {
+                int viTriCu = c - 'A';
+                int viTriMoi = (viTriCu + khoa) % 26;
+                ketQua.append((char) (viTriMoi + 'A'));
+
+            } else {
+                ketQua.append(c);
+            }
+        }
+        banMa = ketQua.toString();
+        return banMa;
+    }
+
+    public String giaiMa() {
+        StringBuilder ketQua = new StringBuilder();
+
+        for (int i = 0; i < banMa.length(); i++) {
+            char c = banMa.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                int viTriCu = c - 'a';
+                int viTriMoi = (viTriCu - khoa + 26) % 26;
+                ketQua.append((char) (viTriMoi + 'a'));
+            } else if (c >= 'A' && c <= 'Z') {
+                int viTriCu = c - 'A';
+                int viTriMoi = (viTriCu - khoa + 26) % 26;
+                ketQua.append((char) (viTriMoi + 'A'));
+            } else {
+                ketQua.append(c);
+            }
+        }
+
+        banRo = ketQua.toString();
+        return banRo;
+    }
+}
