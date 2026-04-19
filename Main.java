77d6023ca9d6b47bffe8928fcd354c3874627e47
Dinh Duc Hoang <149225882+dinhduchoang@users.noreply.github.com>
2026-04-19 15:18:05 +0700

    add main

---
+++ Main.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+//    MaCeasar ceasar = new MaCeasar();
+//    ceasar.setBanRo("NOROSEWITHOUTATH");
+//    ceasar.setKhoa(8);
+//    System.out.println("Ban ma Ceaser : " + ceasar.maHoa());
+//    System.out.println("Ban ro Ceaser : " + ceasar.giaiMa());
+//
+//    Vigenere_LapKhoa v = new Vigenere_LapKhoa();
+//    v.setKhoa("YOUREON");
+//    v.setBanRo("MONEYMAKESTHE");
+//    System.out.println("Ban ma Vigenere_LapKhoa : " + v.maHoa());
+//    System.out.println("Ban ro Vigenere_LapKhoa : " + v.giaiMa());
+//
+//    Vigenere_AutoKey va = new Vigenere_AutoKey();
+//    va.setKhoa("YOUREON");
+//    va.setBanRo("MONEYMAKESTHE");
+//    System.out.println("Ban ma Vigenere_AutoKey : " + va.maHoa());
+//    System.out.println("Ban ro Vigenere_AutoKey : " + va.giaiMa());
+////
+//    MaDon md = new MaDon();
+//    md.setKhoa("THLEYNPSXADWKFUBOGMQVJRCIZ");
+//    md.setBanRo("AWOMANGIVESANDFO");
+//    System.out.println( "Ban ma hoa chu don : " + md.maHoa());
+//    System.out.println( "Ban ro hoa chu don : " + md.giaiMa());
+
+    PlayFair pf = new PlayFair();
+    pf.setKhoa("thedieis");
+    pf.setBanRo("thetruthwillou");
+
+    System.out.println(pf.maHoa());
+    System.out.println(pf.giaiMa());
+
+//    RailFence r = new RailFence();
+//    r.setKhoa(2);
+//    r.setBanRo("meet me after the toga party");
+//    System.out.println(r.maHoa());
+//    System.out.println(r.giaiMa());
+}
